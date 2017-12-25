da8c2917bd40b9dc442790a754df856d813be716
ThangSIC <thangsic@ThangSICs-Mac.local>
2017-12-25 07:38:04 -0800

    Initial Commit

---
+++ ExpanableDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpanableDemo
+//
+//  Created by ThangSIC on 12/25/17.
+//  Copyright © 2017 ThangSIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
